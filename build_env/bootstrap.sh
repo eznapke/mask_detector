b66647e85975ed130ab726d625b032807244e6f1
EPABME <brian.mceniery@ericsson.com>
2020-08-25 06:40:58 +0000

    Update build_env/bootstrap.sh

--- build_env/bootstrap.sh
+++ build_env/bootstrap.sh
@@ -8,8 +8,8 @@ sudo apt-get update -qq
 echo "@@@ $(date): Install docker"
 sudo apt-get install -qq docker docker-compose
 
-echo "@@@ $(date): Install Firefox"
-sudo apt-get install -qq firefox
+# echo "@@@ $(date): Install Firefox"
+# sudo apt-get install -qq firefox
 
 echo "@@@ $(date): Update SSH Service..."
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
