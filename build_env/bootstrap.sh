929a07d030cf72cd25b53c2c70ccaba404f758a0
EPABME <brian.mceniery@ericsson.com>
2020-08-25 01:35:51 +0000

    Update build_env/bootstrap.sh

--- build_env/bootstrap.sh
+++ build_env/bootstrap.sh
@@ -15,8 +15,8 @@ echo "@@@ $(date): Update SSH Service..."
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo service ssh restart
 
-echo "@@@ $(date): GIT...clone the pm_k8s_service"
-git clone https://gitlab.mana.ericssondevops.com/EPABME/pm_k8s_service
-sudo chmod 777 -R /home/vagrant/pm_k8s_service/
+echo "@@@ $(date): GIT...clone the mask_detector"
+git clone https://gitlab.mana.ericssondevops.com/EPABME/mask_detector
+sudo chmod 777 -R /home/vagrant/mask_detector/
 
 echo "@@@ $(date): Bootstrap complete..."
