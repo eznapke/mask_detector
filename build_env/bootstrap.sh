312eda85393b4946d40a5d2b330ea01ededcd665
EPABME <brian.mceniery@ericsson.com>
2020-08-30 02:39:34 +0000

    Update build_env/Vagrantfile, build_env/bootstrap.sh files Deleted build_env/k8s_install.sh

--- build_env/bootstrap.sh
+++ build_env/bootstrap.sh
@@ -8,11 +8,9 @@ sudo apt-get update -qq
 echo "@@@ $(date): Install docker"
 sudo apt-get install -qq docker docker-compose
 
-# echo "@@@ $(date): Install Firefox"
-# sudo apt-get install -qq firefox
-
 echo "@@@ $(date): Update SSH Service..."
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo service ssh restart
 
-echo "@@@ $(date): Bootstrap complete..."
+echo "@@@ $(date): Install Complete"
+echo "@@@ $(date): Your IP Address is $(hostname  -I | cut -f2 -d' ')"
