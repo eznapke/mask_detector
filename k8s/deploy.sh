29f951f42cc1cad9370589267b5cc619f4be358e
EPABME <brian.mceniery@ericsson.com>
2020-09-03 04:49:41 +0000

    Update k8s/install, k8s/deploy.sh files

---
+++ k8s/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+external_ip=$(hostname  -I | cut -f2 -d' ')
+
+echo "@@@ $(date): Create the Namespace"
+microk8s.kubectl create namespace md-service
+
+echo "@@@ $(date): Create the Configmaps"
+microk8s.kubectl create configmap md-service-config --from-env-file=cfg/k8s_md_service_config.properties -n md-service
+
+echo "@@@ $(date): Deploy the Services"
+microk8s.kubectl create -f cfg/k8s_md_service_deployment.yaml -n md-service
+
+echo "@@@ $(date): Expose the Services"
+microk8s.kubectl expose deployment md-service --type=LoadBalancer --name=md-service -n md-service
+microk8s.kubectl patch service md-service  -p '{"spec": {"type": "LoadBalancer", "externalIPs":["'"$external_ip"'"]}}' -n md-service
